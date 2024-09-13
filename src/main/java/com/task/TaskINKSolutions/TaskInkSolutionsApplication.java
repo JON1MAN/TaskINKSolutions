88c00cfbe99cbb05940b459fb6ed69bcbfe58a12
aliaksei <aliaksmarchuk@gmail.com>
2024-09-13 21:56:48 +0200

    created repository

---
+++ src/main/java/com/task/TaskINKSolutions/TaskInkSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.task.TaskINKSolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskInkSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskInkSolutionsApplication.class, args);
+	}
+
+}
