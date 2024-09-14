6b7a56324f0d3a6f181927173f8ce1e9d2864b1d
aliaksei <aliaksmarchuk@gmail.com>
2024-09-15 01:46:20 +0200

    created newsService

--- src/main/java/com/task/TaskINKSolutions/Services/NewsService.java
+++ src/main/java/com/task/TaskINKSolutions/Services/NewsService.java
@@ -1,4 +1,21 @@
 package com.task.TaskINKSolutions.Services;
 
+import com.task.TaskINKSolutions.Entities.News;
+import com.task.TaskINKSolutions.Exceptions.NewsNotFoundException;
+import com.task.TaskINKSolutions.Repositories.NewsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NewsService {
+
+    @Autowired
+    private NewsRepository newsRepository;
+
+    public News getNewsById(Long id){
+        return newsRepository.findNewsById(id)
+                .orElseThrow(() -> new NewsNotFoundException(
+                        "News not found with id: " + id));
+    }
+
 }
