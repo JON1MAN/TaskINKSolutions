cfd5ab133e32acaced906fb0302b30e888b8b41b
aliaksei <aliaksmarchuk@gmail.com>
2024-09-15 01:44:56 +0200

    method to add news to city

--- src/main/java/com/task/TaskINKSolutions/Services/CityService.java
+++ src/main/java/com/task/TaskINKSolutions/Services/CityService.java
@@ -5,8 +5,11 @@ import com.task.TaskINKSolutions.Entities.News;
 import com.task.TaskINKSolutions.Exceptions.CityNotFoundException;
 import com.task.TaskINKSolutions.Repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ssl.SslProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,6 +32,13 @@ public class CityService {
         } else {
             throw new CityNotFoundException("City not found with id: " + id);
         }
+    }
+
+    public void addNewsToCity(News news, Long id){
+        City city = cityRepository.findCityById(id)
+                .orElseThrow(() -> new CityNotFoundException(
+                        "City not found with id: " + id));
+        city.addNews(news);
     }
 
 
