e1f758f4723a470fc36456532a9e528f825cce05
aliaksei <aliaksmarchuk@gmail.com>
2024-09-14 14:29:07 +0200

    added Service for City

---
+++ src/main/java/com/task/TaskINKSolutions/Services/CityService.java
@@ -0,0 +1,35 @@
+package com.task.TaskINKSolutions.Services;
+
+import com.task.TaskINKSolutions.Entities.City;
+import com.task.TaskINKSolutions.Entities.News;
+import com.task.TaskINKSolutions.Exceptions.CityNotFoundException;
+import com.task.TaskINKSolutions.Repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    public City findCityById(Long id){
+        return cityRepository.findCityById(id)
+                .orElseThrow(() -> new CityNotFoundException(
+                        "City not found with id: " + id));
+    }
+
+    public List<News> getAllNewsFromCity(Long id){
+        Optional<City> cityOptional = cityRepository.findCityById(id);
+        if(cityOptional.isPresent()){
+            return cityOptional.get().getNewsFromCity();
+        } else {
+            throw new CityNotFoundException("City not found with id: " + id);
+        }
+    }
+
+
+}
