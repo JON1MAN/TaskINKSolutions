d489913c290ef05ee4c556f16dd06a9b07aeb890
aliaksei <aliaksmarchuk@gmail.com>
2024-09-14 14:31:11 +0200

    Created News Repo

--- src/main/java/com/task/TaskINKSolutions/Repositories/NewsRepository.java
+++ src/main/java/com/task/TaskINKSolutions/Repositories/NewsRepository.java
@@ -1,4 +1,14 @@
 package com.task.TaskINKSolutions.Repositories;
 
-public interface NewsRepository {
+import com.task.TaskINKSolutions.Entities.City;
+import com.task.TaskINKSolutions.Entities.News;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface NewsRepository extends JpaRepository<News, Long> {
+    Optional<News> findNewsById(Long id);
+    Optional<News> findNewsByTitle(String title);
 }
