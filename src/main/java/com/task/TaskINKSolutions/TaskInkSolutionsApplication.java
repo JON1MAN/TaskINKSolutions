b53f42a74e1f96e6665acb50d748641537fdc743
aliaksei <aliaksmarchuk@gmail.com>
2024-09-15 03:46:11 +0200

    initialized method for parsing uscities csv dataset

--- src/main/java/com/task/TaskINKSolutions/TaskInkSolutionsApplication.java
+++ src/main/java/com/task/TaskINKSolutions/TaskInkSolutionsApplication.java
@@ -2,12 +2,20 @@ package com.task.TaskINKSolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class TaskInkSolutionsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskInkSolutionsApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void parseDataSetOfCities() {
+		
+		System.out.println("hello world, I have just started up");
 	}
 
 }
