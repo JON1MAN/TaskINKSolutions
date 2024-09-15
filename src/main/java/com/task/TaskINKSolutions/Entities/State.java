5a056e0360b54f5999707c9b4f1ec95e51b57dc4
aliaksei <aliaksmarchuk@gmail.com>
2024-09-15 04:13:12 +0200

    added lombok annotations

--- src/main/java/com/task/TaskINKSolutions/Entities/State.java
+++ src/main/java/com/task/TaskINKSolutions/Entities/State.java
@@ -1,15 +1,22 @@
 package com.task.TaskINKSolutions.Entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "states")
 public class State {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long stateId;
     String name;
     @OneToMany(mappedBy = "state", cascade = CascadeType.ALL)
