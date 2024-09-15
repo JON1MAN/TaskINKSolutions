0abe6d4b47df8d2ed4288f19b291246bde28db2c
aliaksei <aliaksmarchuk@gmail.com>
2024-09-15 04:38:32 +0200

    created StateService with logic

--- src/main/java/com/task/TaskINKSolutions/Services/StateService.java
+++ src/main/java/com/task/TaskINKSolutions/Services/StateService.java
@@ -1,4 +1,25 @@
 package com.task.TaskINKSolutions.Services;
 
-public class StateRepository {
+import com.task.TaskINKSolutions.Entities.State;
+import com.task.TaskINKSolutions.Exceptions.StateNotFoundException;
+import com.task.TaskINKSolutions.Repositories.StateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StateService {
+
+    @Autowired
+    private StateRepository stateRepository;
+
+    public State findStateById(Long id){
+        return stateRepository.findStateByStateId(id)
+                .orElseThrow(() ->
+                        new StateNotFoundException("State not found with id: " + id)
+                        );
+    }
+
+    public State createState(State state){
+        return stateRepository.save(state);
+    }
 }
