d40b7638e314a51fcd416af86ddb4368ec7bac27
aliaksei <aliaksmarchuk@gmail.com>
2024-09-15 03:26:04 +0200

    State class

--- src/main/java/com/task/TaskINKSolutions/Entities/State.java
+++ src/main/java/com/task/TaskINKSolutions/Entities/State.java
@@ -1,4 +1,18 @@
 package com.task.TaskINKSolutions.Entities;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "states")
 public class State {
+
+    @Id
+    Long stateId;
+    String name;
+    @OneToMany(mappedBy = "state", cascade = CascadeType.ALL)
+    List<News> newsFromState = new ArrayList<>();
+
 }
