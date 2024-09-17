a03b1ab415450a55c4a26944f270f5c5b2c895d5
aliaksei <aliaksmarchuk@gmail.com>
2024-09-17 02:53:56 +0200

    added native query if there more then 1 same cities

--- src/main/java/com/task/TaskINKSolutions/Repositories/CityRepository.java
+++ src/main/java/com/task/TaskINKSolutions/Repositories/CityRepository.java
@@ -2,6 +2,8 @@ package com.task.TaskINKSolutions.Repositories;
 
 import com.task.TaskINKSolutions.Entities.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,8 @@ import java.util.Optional;
 public interface CityRepository extends JpaRepository<City, Long>{
     Optional<City> findCityById(Long id);
     Optional<City> findCityByName(String name);
+
+    @Query(value = "SELECT * FROM cities c WHERE c.name = :cityName AND c.state_id = (SELECT state_id FROM states WHERE name = :stateName) LIMIT 1", nativeQuery = true)
+    City findFirstCityByNameAndState(@Param("cityName") String cityName, @Param("stateName") String stateName);
+
 }
