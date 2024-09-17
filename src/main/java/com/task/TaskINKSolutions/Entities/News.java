6a02473d23b5ee5484372c80730260ed56985803
aliaksei <aliaksmarchuk@gmail.com>
2024-09-17 02:51:41 +0200

    added LocalDate, MAX_LENGTH fields, and condition for description

--- src/main/java/com/task/TaskINKSolutions/Entities/News.java
+++ src/main/java/com/task/TaskINKSolutions/Entities/News.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @Data
@@ -14,6 +15,8 @@ import java.util.Date;
 @Table(name = "news")
 public class News {
 
+    private final static int MAX_LENGTH_FOR_DESCRIPTION = 255;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,7 +24,7 @@ public class News {
     private String title;
     private String description;
     private String url;
-    private Date date;
+    private LocalDate date;
 
     @ManyToOne
     @JoinColumn(name = "city_id", nullable = false)
@@ -30,5 +33,12 @@ public class News {
     @ManyToOne
     @JoinColumn(name = "state_id", nullable = false)
     private State state;
+
+    public void setDescription(String description){
+        if(description.length() > MAX_LENGTH_FOR_DESCRIPTION){
+            description = description.substring(0, MAX_LENGTH_FOR_DESCRIPTION);
+        }
+        this.description = description;
+    }
 
 }
