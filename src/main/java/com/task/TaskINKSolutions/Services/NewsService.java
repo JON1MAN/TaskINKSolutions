d172a040a23e99ae2d3df524f3aa6a687e6eceda
aliaksei <aliaksmarchuk@gmail.com>
2024-09-17 02:55:57 +0200

    added method createNewsFromGPTResponse

--- src/main/java/com/task/TaskINKSolutions/Services/NewsService.java
+++ src/main/java/com/task/TaskINKSolutions/Services/NewsService.java
@@ -1,16 +1,26 @@
 package com.task.TaskINKSolutions.Services;
 
+import com.task.TaskINKSolutions.Entities.City;
 import com.task.TaskINKSolutions.Entities.News;
+import com.task.TaskINKSolutions.Entities.State;
 import com.task.TaskINKSolutions.Exceptions.NewsNotFoundException;
 import com.task.TaskINKSolutions.Repositories.NewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class NewsService {
 
     @Autowired
     private NewsRepository newsRepository;
+    @Autowired
+    private StateService stateService;
+    @Autowired
+    private CityService cityService;
 
     public News getNewsById(Long id){
         return newsRepository.findNewsById(id)
@@ -22,8 +32,43 @@ public class NewsService {
         return newsRepository.save(news);
     }
 
-    public void createNewsFromGPTResponse(String[] fields){
-        
+    public News createNewsFromGPTResponse(String[] fields){
+        String author = fields[0];
+        String title = fields[1];
+        String description = fields[2];
+        String url = fields[3];
+        String date = fields[4].substring(0,10);
+        String typeOfNews = fields[5].toUpperCase();
+        String cityName = fields[6].toUpperCase();
+        String stateName = fields[7].toUpperCase();
+
+        News news = new News();
+        news.setAuthor(author);
+        news.setTitle(title);
+        news.setDescription(description);
+        news.setUrl(url);
+        news.setDate(LocalDate.parse(date));
+
+        if(typeOfNews.equals("GLOBAL")){
+            State state = stateService.findStateByName("GLOBAL");
+            City city = cityService.findCityByName("GLOBAL");
+            news.setCity(city);
+            news.setState(state);
+            state.addNews(news);
+
+            return createNews(news);
+        } else {
+            City city = cityService.findCityByNameAndState(cityName, stateName);
+            State state = stateService.findStateByName(stateName);
+            news.setState(state);
+            news.setCity(city);
+            createNews(news);
+
+            city.addNews(news);
+            state.addNews(news);
+
+            return news;
+        }
     }
 
 }
