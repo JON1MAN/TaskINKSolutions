7934959060142b6e682a4f58bda13bf58df3c2ef
aliaksei <aliaksmarchuk@gmail.com>
2024-09-17 02:57:20 +0200

    parsing of usnews.json file

--- src/main/java/com/task/TaskINKSolutions/TaskInkSolutionsApplication.java
+++ src/main/java/com/task/TaskINKSolutions/TaskInkSolutionsApplication.java
@@ -1,12 +1,18 @@
 package com.task.TaskINKSolutions;
 
 import com.task.TaskINKSolutions.Entities.City;
+import com.task.TaskINKSolutions.Entities.News;
 import com.task.TaskINKSolutions.Entities.State;
 import com.task.TaskINKSolutions.Repositories.CityRepository;
 import com.task.TaskINKSolutions.Services.CityService;
+import com.task.TaskINKSolutions.Services.GptFilterService;
+import com.task.TaskINKSolutions.Services.NewsService;
 import com.task.TaskINKSolutions.Services.StateService;
+import jakarta.servlet.MultipartConfigElement;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,19 +20,105 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class TaskInkSolutionsApplication {
+	private final MultipartConfigElement multipartConfigElement;
+    @Autowired
+    private StateService stateService;
+    @Autowired
+    private CityService cityService;
+
+	public TaskInkSolutionsApplication(MultipartConfigElement multipartConfigElement) {
+		this.multipartConfigElement = multipartConfigElement;
+	}
 
 //	@Autowired
 //	private CityService cityService;
 //	@Autowired
 //	private StateService stateService;
+	@Autowired
+	private GptFilterService gptFilterService;
+	@Autowired
+	private NewsService newsService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskInkSolutionsApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void parseDataSetOfNews() {
+
+		String file = "src/main/resources/static/usnews2.json";
+		try{
+			String jasonContent = "src/main/resources/static/usnews.json";
+			String fileContent = new String(Files.readAllBytes(Paths.get(jasonContent)));
+
+			JSONObject jsonObject = new JSONObject(fileContent);
+			//////////////////////
+			System.out.println(jsonObject);
+
+			JSONArray jsonArray = jsonObject.getJSONArray("articles");
+
+			for(int i = 96; i < jsonArray.length(); i++){
+				//////////////////////
+				System.out.println("ARTICLE NUMBER : " + i);
+
+				JSONObject article = jsonArray.getJSONObject(i);
+				String author = article.optString("author", "author");
+				String title = article.optString("title", "Breaking News");
+				String description = article.optString("description", "Breaking news via link!");
+				String url = article.optString("url", "link");
+				String date = article.optString("date", LocalDate.now().toString());
+				String content = article.optString("content", " ");
+
+				String inputText = String.format
+						(
+								"%s\n%s\n%s\n%s\n%s\n",
+								author,
+								title,
+								description,
+								content,
+								url
+						);
+				//////////////////////
+				System.out.println(inputText);
+
+				State state = stateService.findStateByName("GLOBAL");
+				System.out.println(state.getName());
+
+				JSONObject gptResponse = new JSONObject(
+						gptFilterService.textAnalysis(inputText));
+				//////////////////////
+				System.out.println(gptResponse);
+
+				String contentOfResponse = gptResponse
+						.getJSONObject("message")
+						.getString("content");
+				//////////////////////
+				System.out.println(contentOfResponse);
+				var x = contentOfResponse.split(", ");
+				String[] fields =
+						{author, title, description, url, date,
+								contentOfResponse.split(",")[0].trim(),
+								contentOfResponse.split(",")[1].trim(),
+								contentOfResponse.split(",")[2].trim()
+						};
+				//////////////////////
+				Arrays.stream(fields)
+						.forEach(System.out::println);
+				News news = newsService.createNewsFromGPTResponse(fields);
+
+
+			}
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 //	@EventListener(ApplicationReadyEvent.class)
 //	public void parseDataSetOfCities() {
 //
