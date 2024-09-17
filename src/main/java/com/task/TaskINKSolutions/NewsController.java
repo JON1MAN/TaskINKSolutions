7a53bcca39997d9a836137671d51b0ee0182a8f5
aliaksei <aliaksmarchuk@gmail.com>
2024-09-17 04:56:33 +0200

    added newsController logic

--- src/main/java/com/task/TaskINKSolutions/NewsController.java
+++ src/main/java/com/task/TaskINKSolutions/NewsController.java
@@ -1,4 +1,46 @@
 package com.task.TaskINKSolutions;
 
+import com.task.TaskINKSolutions.Entities.City;
+import com.task.TaskINKSolutions.Entities.News;
+import com.task.TaskINKSolutions.Services.CityService;
+import com.task.TaskINKSolutions.Services.GeolocationService;
+import com.task.TaskINKSolutions.Services.NewsService;
+import com.task.TaskINKSolutions.Services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ap1/v1/news")
 public class NewsController {
+
+    @Autowired
+    private CityService cityService;
+    @Autowired
+    private NewsService newsService;
+    @Autowired
+    private StateService stateService;
+    @Autowired
+    private GeolocationService geolocationService;
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping()
+    public ResponseEntity<String> getNewsFromCityStateGlobal(
+            @RequestParam("latitude") String latitude,
+            @RequestParam("longitude") String longitude
+    ){
+        String cityName = geolocationService.getCityAndState(latitude, longitude);
+        City city = cityService.findCityByName(cityName);
+        List<News> newsFromCity = city.getNewsFromCity();
+        List<News> newsFromState = city.getState().getNewsFromState();
+        List<News> globalNews = stateService.findStateByName("GLOBAL").getNewsFromState()
+                .stream()
+                .limit(2)
+                .toList();
+
+        return ResponseEntity.ok(cityName);
+    }
+
 }
