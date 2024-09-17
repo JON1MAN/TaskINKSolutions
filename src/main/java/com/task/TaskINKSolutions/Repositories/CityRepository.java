b3064ca77ab3e099c9209b2ff9b1894a345ae666
aliaksei <aliaksmarchuk@gmail.com>
2024-09-17 04:57:17 +0200

    changed query to get first city if there are same

--- src/main/java/com/task/TaskINKSolutions/Repositories/CityRepository.java
+++ src/main/java/com/task/TaskINKSolutions/Repositories/CityRepository.java
@@ -11,7 +11,8 @@ import java.util.Optional;
 @Repository
 public interface CityRepository extends JpaRepository<City, Long>{
     Optional<City> findCityById(Long id);
-    Optional<City> findCityByName(String name);
+    @Query(value = "SELECT * FROM cities c WHERE c.name = :cityName LIMIT 1", nativeQuery = true)
+    Optional<City> findCityByName(@Param("cityName") String cityName);
 
     @Query(value = "SELECT * FROM cities c WHERE c.name = :cityName AND c.state_id = (SELECT state_id FROM states WHERE name = :stateName) LIMIT 1", nativeQuery = true)
     City findFirstCityByNameAndState(@Param("cityName") String cityName, @Param("stateName") String stateName);
